328e3c0fb8a447bfe89cb1b8b5bd880513a2f5f1
hobbyco <pdongsoo@naver.com>
2025-08-21 11:14:52 +0900

    Add files via upload

---
+++ mini_games/climbing_2048/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'bouldering-2048-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 서비스 워커 설치
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('캐시 열림');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 캐시된 리소스 제공
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 캐시에서 반환, 없으면 네트워크에서 가져오기
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 오래된 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
